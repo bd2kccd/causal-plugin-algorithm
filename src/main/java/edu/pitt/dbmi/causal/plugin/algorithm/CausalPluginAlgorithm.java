00f2f615b9330857bf37461160b6e399ca9dd1de
chirayukong <chirayu.kong@gmail.com>
2018-10-04 18:47:24 -0400

    Added Gson

--- src/main/java/edu/pitt/dbmi/causal/plugin/algorithm/CausalPluginAlgorithm.java
+++ src/main/java/edu/pitt/dbmi/causal/plugin/algorithm/CausalPluginAlgorithm.java
@@ -1,6 +1,7 @@
 package edu.pitt.dbmi.causal.plugin.algorithm;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.pf4j.Extension;
@@ -8,12 +9,17 @@ import org.pf4j.Plugin;
 import org.pf4j.PluginException;
 import org.pf4j.PluginWrapper;
 
+import com.google.gson.Gson;
+
 import edu.cmu.tetrad.algcomparison.algorithm.Algorithm;
+import edu.cmu.tetrad.algcomparison.score.BdeuScore;
 import edu.cmu.tetrad.algcomparison.score.ScoreWrapper;
 import edu.cmu.tetrad.algcomparison.utils.HasKnowledge;
 import edu.cmu.tetrad.algcomparison.utils.TakesInitialGraph;
 import edu.cmu.tetrad.algcomparison.utils.UsesScoreWrapper;
 import edu.cmu.tetrad.annotation.AlgType;
+import edu.cmu.tetrad.data.BoxDataSet;
+import edu.cmu.tetrad.data.CovarianceMatrixOnTheFly;
 import edu.cmu.tetrad.data.DataModel;
 import edu.cmu.tetrad.data.DataType;
 import edu.cmu.tetrad.data.IKnowledge;
@@ -21,6 +27,7 @@ import edu.cmu.tetrad.data.Knowledge2;
 import edu.cmu.tetrad.graph.EdgeListGraph;
 import edu.cmu.tetrad.graph.Graph;
 import edu.cmu.tetrad.search.SearchGraphUtils;
+import edu.cmu.tetrad.util.JsonUtils;
 import edu.cmu.tetrad.util.Parameters;
 import edu.pitt.dbmi.tetrad.plugin.api.CausalPlugin;
 
@@ -62,6 +69,8 @@ public class CausalPluginAlgorithm extends Plugin {
 		private Algorithm algorithm = null;
 		private Graph initialGraph = null;
 		private IKnowledge knowledge = new Knowledge2();
+		
+		private Gson gson = new Gson();
 
 		@Override
 		public Graph search(DataModel dataSet, Parameters parameters) {
@@ -88,6 +97,29 @@ public class CausalPluginAlgorithm extends Plugin {
 			return search.search();
 		}
 
+		public Graph search(String dataSetJson, String parametersJson) {
+			DataModel dataSet = null;
+			Parameters parameters = null;
+			parameters = gson.fromJson(parametersJson, Parameters.class);
+						
+			//dataSet = JsonUtils.parseJSONObjectToTetradGraph(dataSetJson);
+			
+			DataType dataType = getDataType();
+			switch(dataType) {
+			case Continuous:
+			case Discrete:
+			case Mixed:
+				dataSet = gson.fromJson(dataSetJson, BoxDataSet.class);
+				break;
+			case Covariance:
+				dataSet = gson.fromJson(dataSetJson, CovarianceMatrixOnTheFly.class);
+			default:
+				break;
+			}
+			
+			return search(dataSet, parameters);
+		}
+		
 		@Override
 		public Graph getComparisonGraph(Graph graph) {
 			if (compareToTrue) {
@@ -109,7 +141,12 @@ public class CausalPluginAlgorithm extends Plugin {
 
 		@Override
 		public List<String> getParameters() {
-			List<String> parameters = score.getParameters();
+			List<String> parameters;
+			try {
+				parameters = score.getParameters();
+			} catch (Exception e) {
+				parameters = new ArrayList<>();
+			}
 			parameters.add("faithfulnessAssumed");
 			parameters.add("symmetricFirstStep");
 			parameters.add("maxDegree");
@@ -142,9 +179,36 @@ public class CausalPluginAlgorithm extends Plugin {
 			this.knowledge = knowledge;
 		}
 
+		public void setKnowledge(String knowledge) {
+			System.out.println("knowledge: " + knowledge);
+			IKnowledge iknowledge = gson.fromJson(knowledge, Knowledge2.class);
+			setKnowledge(iknowledge);
+		}
+
 		@Override
 		public void setScoreWrapper(ScoreWrapper score) {
 			this.score = score;
+		}
+
+		public void setScoreWrapper(BdeuScore score) {
+			this.score = (ScoreWrapper)score;
+		}
+
+		public void setScoreWrapper(String scoreClassName) {
+			try {
+				Class clazz =  Class.forName(scoreClassName);
+				Object score = clazz.newInstance();
+				this.score = (ScoreWrapper)score;
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		public void setCompareToTrue(boolean compareToTrue) {
